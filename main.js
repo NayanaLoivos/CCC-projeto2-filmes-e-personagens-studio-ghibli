f5ded9b92e734881448a122e2e0fc88503402d39
Nayana Loivos <loivosdev@gmail.com>
2022-03-09 22:39:38 -0300

    fetch, json e html dinâmico

--- main.js
+++ main.js
@@ -1,9 +1,35 @@
-const filmesStudioGhibli = require("./ghibli");
-//console.log(filmesStudioGhibli);
+
+const urlJason = "https://raw.githubusercontent.com/NayanaLoivos/CCC-projeto2-filmes-e-personagens-studio-ghibli/main/ghibli.json"
+const h1Title = document.getElementById('jsontitle');
+const jsonPoster = document.getElementById('jsonposter'); //não consigo usar
 
 
-let nomesFilmes = [];
-for(let info in filmesStudioGhibli.films){
-    nomesFilmes.push(filmesStudioGhibli.films[info].title);
+fetch(urlJason)
+    .then(dados => dados.json())     
+    .then(resposta => {
+         resposta.films.forEach(film => 
+            criarFilmeETituloNoHTML(film)
+             )
+    })       
+
+function criarFilmeETituloNoHTML(film){
+    h1Title.innerHTML+=(`<h3>${film.title}</h3>`)
+    obterImagem(film.poster)
+}    
+
+function obterImagem(url){
+    fetch(url)
+    .then(dados => dados.blob())
+    //.then(resposta => console.log(resposta))
+    .then(resposta => {
+        const imgURL = URL.createObjectURL(resposta)
+        const img = `<img src="${imgURL}"/>` 
+    
+        h1Title.innerHTML+=(img)
+    });
 }
- 
+    
+        
+    
+  
+    
